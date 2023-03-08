7527befc1920029629af1c34cc4f87163c306e01
Batap <batapxv7@gmail.com>
2023-03-08 21:26:58 +0100

    TP2 fini

---
+++ TP1_2/TP1_Code/build/launch-TP1.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+bindir=$(pwd)
+cd /run/media/batap/HDD_2To/Dossier_Bat/Fac_CV/fac/M1/S2/HAI819I_Moteur_de_Jeux/TP2/TP1/
+export 
+
+if test "x$1" = "x--debugger"; then
+	shift
+	if test "xYES" = "xYES"; then
+		echo "r  " > $bindir/gdbscript
+		echo "bt" >> $bindir/gdbscript
+		/usr/bin/gdb -batch -command=$bindir/gdbscript --return-child-result /run/media/batap/HDD_2To/Dossier_Bat/Fac_CV/fac/M1/S2/HAI819I_Moteur_de_Jeux/TP2/build/TP1 
+	else
+		"/run/media/batap/HDD_2To/Dossier_Bat/Fac_CV/fac/M1/S2/HAI819I_Moteur_de_Jeux/TP2/build/TP1"  
+	fi
+else
+	"/run/media/batap/HDD_2To/Dossier_Bat/Fac_CV/fac/M1/S2/HAI819I_Moteur_de_Jeux/TP2/build/TP1"  
+fi
---
+++ TP1_2/TP1_Code/cmake-build-debug/CMakeFiles/launch-TP1.cmd
@@ -0,0 +1,9 @@
+cd E:/Dossier_Bat/Fac_CV/fac/M1/S2/HAI819I_Moteur_de_Jeux/TP2/TP1/
+set 
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"E:\Dossier_Bat\Fac_CV\fac\M1\S2\HAI819I_Moteur_de_Jeux\TP2\cmake-build-debug\TP1.exe"  
+pause
